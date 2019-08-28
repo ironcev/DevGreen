cd6fb4ca611fb578b56e4264556131718fc42195
Igor Rončević <ironcev@hotmail.com>
2019-08-28 19:24:35 +0200

    refactor: Change changeBarsToSucceeded to changeAllBarsToSucceeded

--- dev-green-bookmarklet.js
+++ dev-green-bookmarklet.js
@@ -1,12 +1,11 @@
-function changeBarsToSucceeded(barType) {
-    var bars = Array.from(document.getElementsByClassName('bar ' + barType + ' interactive'));
-    for (var i = 0; i < bars.length; i++) {
-        bars[i].classList.remove(barType);
-        bars[i].classList.add('succeeded');
-    }
-}
-(function main() {
-   changeBarsToSucceeded('failed');
-   changeBarsToSucceeded('partiallysucceeded');
-   changeBarsToSucceeded('canceled');
+(function changeAllBarsToSucceeded() {
+    var unsuccessfulBarTypes = ['failed', 'partiallysucceeded', 'canceled'];
+    
+    unsuccessfulBarTypes.forEach(function (barType) {
+        var bars = Array.from(document.getElementsByClassName('bar ' + barType + ' interactive'));
+        for (var i = 0; i < bars.length; i++) {
+            bars[i].classList.remove(barType);
+            bars[i].classList.add('succeeded');
+        }
+    });
 })();
